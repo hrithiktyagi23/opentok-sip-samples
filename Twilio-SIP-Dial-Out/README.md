b22eac56871c85c680bfd9ffe42d45ca58860658
Manik <msach22@gmail.com>
2017-12-06 10:41:14 -0800

    Add twilio-sip-dial-out sample app

---
+++ Twilio-SIP-Dial-Out/README.md
@@ -0,0 +1,45 @@
+# Twilio-SIP-Dial-Out
+
+  This app shows how to connect to an OpenTok session, publish a stream, subscribe to **multiple streams**, use OpenTok SIP Interconnect with Twilio to call a phone number.
+
+## Configuring the application
+
+Before running the application, you need to configure the following credentials:
+  * OpenTok
+    * OpenTok Api Key
+    * OpenTok Api Secret
+  * Twilio
+    * Sip URI
+    * Authorized Username for the SIP URI
+    * Authorized Password for the SIP URI
+    * Caller ID (phone number) verified by Twilio
+
+Open the `config.js` file in your project and set the `apiKey`, `apiSecret`, `sip.uri`, `sip.username`, `sip.password`, and `callerId` values.
+
+```
+  module.exports = {
+    apiKey: '',
+    apiSecret: '',
+    sip: {
+      uri: '',
+      username: '',
+      password: '',
+    },
+    callerId: '',
+  };
+```
+
+## Setting up OpenTok & Twilio projects
+  For OpenTok:
+  * Create an API Project to get the API Key and Secret.
+
+  For Twilio:
+  * Create a SIP URI Endpoint and authorize a set of credentials for the SIP URI. 
+    * Make sure to check `ENABLED` under SIP Registration.
+    *   | Config      |   Option          |  URL  | Request Type  |
+        | :-------------: |:-------------:| :-----:| :-----:|
+        | REQUEST URL | Webhook | https://your-server.com/voice | GET |
+  * Make sure to verify your phone number with Twilio so it can be used as the caller Id. 
+
+## Starting the application
+`npm start`
---
+++ Twilio-SIP-Dial-Out/index.js
@@ -0,0 +1,178 @@
+const express = require('express');
+const OpenTok = require('opentok');
+const config = require('./config');
+const twilio = require('twilio');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const OT = new OpenTok(config.apiKey, config.apiSecret);
+
+/**
+   * endCall is used to end the phone call by invoking the hangup method
+   * in Twilio's API
+*/
+
+const endCall = () => {
+  const { VoiceResponse } = twilio.twiml;
+  const response = new VoiceResponse();
+  response.hangup();
+  console.log(response.toString());
+};
+
+/**
+   * generateToken is used to create a token for a user
+   * @param {String} sessionId
+   * @param {String} sipTokenData
+*/
+
+const generateToken = (sessionId, sipTokenData = '') => OT.generateToken(sessionId, {
+  role: 'publisher',
+  data: sipTokenData,
+});
+
+/**
+   * renderRoom is used to render the ejs template
+   * @param {Object} res
+   * @param {String} sessionId
+   * @param {String} token
+   * @param {String} roomId
+*/
+
+const renderRoom = (res, sessionId, token, roomId) => {
+  res.render('index.ejs', {
+    apiKey: config.apiKey,
+    sessionId,
+    token,
+    roomId,
+  });
+};
+
+/**
+   * setSessionDataAndRenderRoom is used to create an OpenTok session & create a token
+   * It's to be used only once per roomId
+   * @param {Object} res
+   * @param {String} roomId
+*/
+
+const setSessionDataAndRenderRoom = (res, roomId) => {
+  OT.createSession({
+    mediaMode: 'routed',
+  }, (error, session) => {
+    const token = generateToken(session.sessionId);
+    app.set(roomId, session.sessionId);
+    renderRoom(res, session.sessionId, token, roomId);
+  });
+};
+
+/**
+   * setSipOptions is used to set properties for the OT.dial API call
+   * @param {String} roomId
+   * @param {Number} pinCode
+   * @param {String} conferenceNumber
+*/
+
+const setSipOptions = (roomId, conferenceNumber) => ({
+  headers: {
+    'X-PH-ROOMNAME': encodeURIComponent(roomId),
+    'X-PH-DIALOUT-NUMBER': conferenceNumber,
+  },
+  auth: {
+    username: config.sip.username,
+    password: config.sip.password,
+  },
+});
+
+/**
+   * startCall is used initiate the phone call using Twilio's API
+   * @param {Object} res
+   * @param {String} phoneNumber
+*/
+const startCall = (res, phoneNumber) => {
+  const { VoiceResponse } = twilio.twiml;
+  const response = new VoiceResponse();
+  const dial = response.dial({
+    callerId: config.callerId,
+  });
+  dial.number(phoneNumber);
+  response.say('The call has ended');
+  res.send(response.toString());
+};
+
+/**
+   * When the room/:rid request is made, either renderRoom or setSessionDataAndRenderRoom
+   * function is called depending on if the roomId exists in memory
+*/
+
+app.get('/room/:rid', (req, res) => {
+  const roomId = req.params.rid;
+  if (app.get(roomId)) {
+    const sessionId = app.get(roomId);
+    const token = generateToken(sessionId);
+    renderRoom(res, sessionId, token, roomId);
+  } else {
+    setSessionDataAndRenderRoom(res, roomId);
+  }
+});
+
+/**
+   * When the dial-out get request is made, the dial method of the OpenTok Dial API is invoked
+*/
+
+app.get('/dial-out', (req, res) => {
+  const { roomId, phoneNumber } = req.query;
+  const sipTokenData = `{"sip":true, "role":"client", "name":"'${phoneNumber}'"}`;
+  const sessionId = app.get(roomId);
+  const token = generateToken(sessionId, sipTokenData);
+  const options = setSipOptions(roomId, phoneNumber);
+  OT.dial(sessionId, token, config.sip.uri, options, (error, sipCall) => {
+    if (error) {
+      return res.json(error);
+    }
+    return res.json(sipCall);
+  });
+});
+
+/**
+   * When the hang-up get request is made, the forceDisconnect method of the OpenTok API is invoked
+*/
+
+app.get('/hang-up', (req, res) => {
+  const roomId = req.query.roomId || '';
+  const connectionId = req.query.connectionId || '';
+  if (app.get(roomId)) {
+    const sessionId = app.get(roomId);
+    OT.forceDisconnect(sessionId, connectionId, (error) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+      endCall();
+      return res.send({
+        hangUp: true,
+      });
+    });
+  } else {
+    res.send({
+      error: 'Nothing to hangup',
+    });
+  }
+});
+/**
+   * When the voice get request is made, the startCall function is called
+*/
+app.get('/voice', (req, res) => {
+  console.log('voice initiated');
+  const phoneNumber = req.query['SipHeader_X-PH-DIALOUT-NUMBER'];
+  startCall(res, phoneNumber);
+});
+
+const port = process.env.PORT || '3000';
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
